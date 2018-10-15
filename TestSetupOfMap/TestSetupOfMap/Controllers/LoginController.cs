acb8eeeafb6d663b689c3e0f78db9c4156fe51be
Levi Pomeroy <levipomeroy@yahoo.com>
2018-10-14 17:40:03 -0700

    Added layout and controllers for login and registatration pages and
    Added controllers and changed routing.

--- TestSetupOfMap/TestSetupOfMap/Controllers/LoginController.cs
+++ TestSetupOfMap/TestSetupOfMap/Controllers/LoginController.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI;
 
 namespace TestSetupOfMap.Controllers
 {
-    public class HomeController : Controller
+    public class LoginController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Register()
         {
             return View();
         }
--- TestSetupOfMap/TestSetupOfMap/Controllers/RegisterController.cs
+++ TestSetupOfMap/TestSetupOfMap/Controllers/RegisterController.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.UI;
 
 namespace TestSetupOfMap.Controllers
 {
-    public class HomeController : Controller
+    public class RegisterController : Controller
     {
+        // GET: Register
         public ActionResult Index()
         {
             return View();
