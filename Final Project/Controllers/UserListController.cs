09e207378b0dfa1b79038fae76948e93a7e93d98
Levi Pomeroy <levipomeroy@yahoo.com>
2018-11-14 12:28:01 -0800

    Added basic list page template

---
+++ Final Project/Controllers/UserListController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Final_Project.Controllers
+{
+    public class UserListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Final Project/DB.sql
+++ Final Project/DB.sql
@@ -40,9 +40,11 @@ Select * from Locations;
 go
 --Test Locations
 delete from Locations;
+go
 insert into Locations(Country, StateCode, City, ZipCode, Address, Latitude, Longitude, Name, Description, Category)
 values('United States', 'OR', 'Klamath Falls', '97603', '3201 Campus Dr', 42.254300, -121.782270, 'OIT', 'My School', 1)
-
+go
 insert into Locations(Country, StateCode, City, ZipCode, Address, Latitude, Longitude, Name, Description, Category)
 values('United States', 'DC', 'Washington DC', '20500', '1600 Pennsylvania Ave NW', 38.897663, -77.036575, 'The White House', 'Where the president lives', 2)
+go
 select * from Locations;
